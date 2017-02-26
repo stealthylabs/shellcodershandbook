0e7ff967a776686b292115f118db96538c97ec45
Stealthy Labs <github@stealthylabs.com>
2017-02-25 23:26:21 -0500

    handling wow64 processes for checking Ntglobalflag

--- windows/antidebug.c
+++ windows/antidebug.c
@@ -10,6 +10,7 @@ int main()
 {
     int flag = 0;
 #ifdef USE_X86_INLINE
+    BOOL iswow64 = FALSE;
     __asm {
         mov eax, fs:[0x30] ; Process Environment Block
         mov al, [eax + 0x68]; NtGlobalFlag
@@ -18,12 +19,31 @@ int main()
         je label1
         xor eax, eax
         mov flag, eax
-        jmp endlabel
+        jmp endlabel1
     label1:
         xor eax, eax
         inc eax
         mov flag, eax
-    endlabel:
+    endlabel1:
+    }
+    if (IsWow64Process(GetCurrentProcess(), &iswow64) && iswow64) {
+        int flag32on64 = 0;
+        __asm {
+            mov eax, fs:[0x30] ; Process Environment Block
+            mov al, [eax + 0x10bc]; NtGlobalFlag in 64-bit PEB
+            and al, 0x70
+            cmp al, 0x70
+            je label2
+            xor eax, eax
+            mov flag, eax
+            jmp endlabel2
+        label2:
+            xor eax, eax
+            inc eax
+            mov flag32on64, eax
+        endlabel2:
+        }
+        printf("flag32on64: %d\n", flag32on64);
     }
 #endif
 #ifdef USE_X64_CALL
